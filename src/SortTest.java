bfccd1733a4adc279ba655d700a8f37fab27eafa
Colin Swan <colin.swan@pitt.edu>
2016-03-21 20:34:59 -0400

    initial commit

---
+++ src/SortTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SortTest{
+	/*
+	 * Given I am on the homepage
+	 * when I click the button for the most recent Gaming Discussion post
+	 * then I should be sent to a thread in the Gaming or Gaming Community sub-forum.
+	*/
+	@Test
+	public void testSameSize(){
+		
+	}
+	
+	@Test
+	public void testOutputIncreasing(){
+		
+	}
+	
+	@Test
+	public void testIdempotent(){
+	}
+}
